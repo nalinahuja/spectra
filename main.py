be5b1b0c4079cebe15e546b1d9b188e318037eec
nalinahuja22 <nalinahuja22@gmail.com>
2019-07-23 11:53:27 -0700

    Project Backup

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import image
 
+#Analyze Images
 grp1 = image.analyze("./images/tests/augmentation")
 
+#Print Scenes Detected
 print("*" + str(grp1.detect_scenes()) + " scenes detected*")
+
+#Print Duplicate Count
 print("\n*Possible Duplicates*")
 
 dup_arr = grp1.detect_duplicates()
