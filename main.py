5a06a5c066b638e99ed5935583b65621007c9a42
Nalin Ahuja <apple@mbair.local>
2019-08-23 17:19:54 -0400

    Project Backup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import image
 
 #Analyze Images
-grp1 = image.analyze("../../Downloads")
+grp1 = image.analyze("./tests/color_shift")
 
 #Print Scenes Detected
 print("*" + str(grp1.detect_scenes()) + " scenes detected*")
