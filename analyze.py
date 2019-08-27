faa8ff84473ca41ba2f6b7b604f1466ee2245995
Nalin Ahuja <apple@mbair.local>
2019-08-27 13:10:52 -0400

    Project Backup

--- analyze.py
+++ analyze.py
@@ -2,12 +2,15 @@ import os
 import sys
 import image
 
-try:
+def analyze(dir, scene_threshold = None, duplicate_threshold = None, blur_threshold = None):
+    #Global Assignment
+    global image
+
     #Analyze Images
-    image_grp = image.analyze(os.path.normpath(sys.argv[1]))
+    image_grp = image.analyze(os.path.normpath(dir))
 
     #Print Scenes Detected
-    print("*" + str(image_grp.detect_scenes()) + " scenes detected*")
+    print("*" + str(image_grp.detect_scenes(threshold = scene_threshold)) + " scenes detected*")
 
     #Print Hashing Values
     if (not(len(image_grp.hash_diffs) == 0)):
@@ -18,17 +21,41 @@ try:
             cnt += 1
 
     #Print Duplicate Array
-    if (not(len(image_grp.detect_duplicates()) == 0)):
+    if (not(len(image_grp.detect_duplicates(threshold = duplicate_threshold)) == 0)):
         print("\n*Possible Duplicate Images*")
         for scene in image_grp.detect_duplicates():
             print(scene)
 
     #Print Blur Array
-    if (not(len(image_grp.detect_blur()) == 0)):
+    if (not(len(image_grp.detect_blur(threshold = blur_threshold)) == 0)):
         print("\n*Possible Blurry Images*")
         for image in image_grp.detect_blur():
             print(image)
-except IndexError:
-    print("ERROR: Please Indicate a Directory!")
-except FileNotFoundError:
-    print("ERROR: Invalid Directory!")
+
+if __name__ == "__main__":
+    data_in = []
+
+    #Param Ingest
+    try:
+        data_in.append(sys.argv[1])
+        data_in.append(sys.argv[2])
+        data_in.append(sys.argv[3])
+        data_in.append(sys.argv[4])
+
+    #Missing Param Catch
+    except IndexError:
+        if (len(data_in) == 0):
+            print("ERROR: Please Indicate a Directory!")
+        elif (len(data_in) == 1):
+            analyze(data_in[0])
+        elif (len(data_in) == 2):
+            analyze(data_in[0], scene_threshold = data_in[1])
+        elif (len(data_in) == 3):
+            analyze(data_in[0], scene_threshold = data_in[1], duplicate_threshold = data_in[2])
+
+    #Invlaid File Location Catch
+    except FileNotFoundError:
+        print("ERROR: Invalid Directory!")
+
+    #Complete Param Analyze Call
+    analyze(data_in[0], scene_threshold = data_in[1], duplicate_threshold = data_in[2], blur_threshold = data_in[3])
