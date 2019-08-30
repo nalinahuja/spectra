748a7317eb302e71d9ed19d5b85cc42e749a4947
Nalin Ahuja <apple@mbair.local>
2019-08-30 10:38:46 -0400

    Project Backup

--- install.sh
+++ install.sh
@@ -8,22 +8,13 @@ then
   exit 1
 fi
 
-echo -e "\nPlease Provide Admin Privileges..."
-sudo apt update
-
 if [ $version == 2.x ]
 then
-  echo -e "\nInstalling pip Installer..."
-  sudo apt install python-pip
-
   echo -e "\nInstalling Python Dependencies..."
   pip install cv2
   pip install imagehash
 elif [ $version == 3.x ]
 then
-  echo -e "\nInstalling pip3 Installer..."
-  sudo apt install python3-pip
-
   echo -e "\nInstalling Python Dependencies..."
   pip3 install cv2
   pip3 install imagehash
